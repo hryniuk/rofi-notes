e6aafa9624e5662c4db79398cfdfb0e2709b3237
Łukasz Hryniuk <lukasz.hryniuk@wp.pl>
2015-12-28 02:18:07 +0100

    Initial commit

---
+++ rofi_notes.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+NOTES_FILE=~/.rofi_notes
+
+if [ ! -a "${NOTES_FILE}" ]
+then
+    touch "${NOTES_FILE}"
+    echo "empty" >> "${NOTES_FILE}"
+fi
+
+function get_notes()
+{
+    cat ${NOTES_FILE}
+}
+
+ALL_NOTES="$(get_notes)"
+
+NOTE=$( (echo "${ALL_NOTES}")  | rofi -dmenu -p "Note:")
+MATCHING=$( (echo "${ALL_NOTES}") | grep "^${NOTE}$")
+
+if [ -n "${MATCHING}" ]
+then
+    NEW_NOTES=$( (echo "${ALL_NOTES}")  | grep -v "^${NOTE}$" )
+else
+    NEW_NOTES=$(echo -e "${ALL_NOTES}\n${NOTE}")
+fi
+
+echo "${NEW_NOTES}" > "${NOTES_FILE}"
